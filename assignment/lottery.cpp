cad10443496ca8591d5212454c8fe58ca64d38dc
danielandricic <danielandricic@hotmail.de>
2019-04-10 17:31:41 +0200

    Work in Progress

--- assignment/lottery.cpp
+++ assignment/lottery.cpp
@@ -1,12 +1,38 @@
 /*-----------------------------------------------------------------------------
- *				HTBLA-Leonding / Class: <your class name here>
+ *				HTBLA-Leonding / Class: 2AHIF
  *-----------------------------------------------------------------------------
- * Exercise Number: #exercise_number#
+ * Exercise Number: 20
  * File:			lottery.c
- * Author(s):		Peter Bauer
- * Due Date:		#due#
+ * Author(s):		Daniel Andricic
+ * Due Date:		09.04.2019
  *-----------------------------------------------------------------------------
  * Description:
  * Implementation of a library for analyzing lottery tips.
  *-----------------------------------------------------------------------------
 */
+#include "lottery.h"
+
+bool init_lottery(const char *csv_file, char csv_separator)
+{
+  return true;
+}
+
+bool 	get_tip (int tip_number, int tip[TIP_SIZE])
+{
+  return true;
+}
+
+bool 	set_drawing (int drawing_numbers[TIP_SIZE])
+{
+  return true;
+}
+
+int 	get_tip_result (int tip_number)
+{
+  return 0;
+}
+
+int 	get_right_tips_count (int right_digits_count)
+{
+  return 0;
+}
